67ae945044895cb557574eab248d85db9fb8748e
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-11 13:29:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { MantineProvider, Text } from '@mantine/core';
+import {HeaderMenu} from './components/Header/Header'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+
