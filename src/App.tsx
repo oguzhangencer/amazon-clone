62c13a052b4336c7cc34599130a32984af332594
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-12 18:32:29 +0300

    Finish navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Products from './components/Products';
-import Cart from './components/Cart';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 export default function App() {
   return (
-  
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>  
-    
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
-
-
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,107 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Button, Select, Text, TextInput } from "@mantine/core";
+import { BsList, BsCart3, BsSearch, BsGeoAlt } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <>
-      <div className='bg-zinc-900 flex justify-between h-20 '>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="flex flex-col">
+      {/* Navbar Up */}
+      <div className=" bg-gray-900 h-14 flex items-center justify-between py-2 px-4 gap-x-4">
+        {/* Navbar Left */}
+        <div className="flex justify-center items-center h-full gap-x-4">
+          {/* Amazon Logo */}
+          <Link to="/">
+            <img
+              className="w-20 "
+              src="/images/amazonLogo.png"
+              alt="amazonLogo"
+            />
+          </Link>
+          <div className="flex items-center gap-x-1 outline hover:outline-white">
+            {/* Select Country */}
+            <BsGeoAlt color="white" />
+            <div className="flex flex-col ">
+              <Text className="text-sm font-thin text-left text-white">
+                Deliver to
+              </Text>
+              <Text className="text-sm font-bold text-white">Turkey</Text>
+            </div>
+          </div>
+        </div>
+        {/* Navbar Middle */}
+        <div className="flex items-center grow outline focus:outline-amber-500 ">
+          {/* Select */}
+          <Select
+            className="flex flex-col "
+            placeholder="All"
+            data={[
+              { value: "react", label: "kashgfgsalfgasfhalshfl" },
+              { value: "ng", label: "Angular" },
+              { value: "svelte", label: "Svelte" },
+              { value: "vue", label: "Vue" },
+            ]}
+          />
+          {/* Text input */}
+          <TextInput
+            className="text-select w-full"
+            placeholder="Your name"
+            styles={{
+              input: {
+                borderRadius: 0,
+              },
+            }}
+          />
+          {/* Search Button */}
+          <Button
+            radius="xs"
+            className="rounded-r-lg bg-amber-500 hover:bg-amber-600"
+          >
+            <BsSearch />
+          </Button>
+        </div>
+        {/* Navbar Right */}
+        <div className="flex justify-center items-center h-full gap-x-6 ">
+          {/* Country */}
+          <div className="flex flex-col outline hover:outline-white">
+            <Text className="text-sm font-bold text-white">EN</Text>
+          </div>
+          {/* Sign in  or  Log in */}
+          <div className="flex flex-col  outline hover:outline-white">
+            <Text className="text-sm font-thin text-left text-white">
+              Hello, sign in
+            </Text>
+            <Text className="text-sm font-bold text-white">
+              Account & Lists
+            </Text>
+          </div>
+          {/* Orders */}
+          <div className="flex flex-col outline hover:outline-white">
+            <Text className="text-sm font-thin text-left text-white">
+              Returns
+            </Text>
+            <Text className="text-sm font-bold text-white">& Orders</Text>
+          </div>
+          {/* Cart */}
+          <div className="flex items-center gap-x-1 outline">
+            <BsCart3 color="white" size={30} />
+            <div className="flex flex-col">
+              <Text className="text-sm font-bold text-white">Cart</Text>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
-  )
+      {/* Navbar Down */}
+      <div className="bg-slate-800 flex items-center py-1 px-4 gap-x-4">
+        <BsList color="white" size={30} />
+        <>
+          <Text className="text-sm font-bold text-white">All</Text>
+          <Text className="text-white">Today's Deals</Text>
+          <Text className="text-white"> Customer Service</Text>
+          <Text className="text-white">Registry</Text>
+          <Text className="text-white">Gift Cards</Text>
+          <Text className="text-white">Sell</Text>
+        </>
+      </div>
+    </div>
+  );
 }
