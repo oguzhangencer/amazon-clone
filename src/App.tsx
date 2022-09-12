d28363dd68e36e0129cf32bbe5dd2b4fed62e420
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-12 11:21:46 +0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import { MantineProvider, Text } from '@mantine/core';
-import {HeaderMenu} from './components/Header/Header'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Products from './components/Products';
+import Cart from './components/Cart';
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+  
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>  
+    
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <div className='bg-zinc-900 flex justify-between h-20 '>
+
+      </div>
+    </>
+  )
+}
