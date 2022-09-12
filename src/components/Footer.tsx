454abcc2938b94acccead06396697e1076635ae7
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-12 12:15:29 +0300

    Footer Sizing & Color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      Footer
+    <div className='absolute inset-x-0 bottom-0'>
+      <div className='bg-slate-800 h-52'>
+        up
+      </div>
+      <div className='bg-gray-900 h-52'>
+        down
+      </div>
+      
     </div>
   )
 }
