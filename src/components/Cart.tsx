a35ccad188f62486038ea7f85df132301e7eb548
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-14 17:55:30 +0300

    Adding Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,6 @@
-import products from "../data/products.json";
-import { Col, Text } from "@mantine/core";
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+import homeDecore from "../data/homeDecore.json";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return <div className="grid grid-cols-1 gap-x-10 gap-y-10 py-10"></div>;
 }
