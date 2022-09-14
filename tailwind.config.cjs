4618002d0084adbb6eadf2ac50403ce6e3b2c077
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-14 07:46:32 +0300

    Footer Padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '500px': '500px',
+      }
+    },
   },
   plugins: [],
 }
