402fec9c705e0b50fdbfdaaa581508795fb38ae3
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-19 13:22:14 +0300

    Added CartState

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,24 @@
-import products from "../data/products.json";
-import ProductCart from "./ProductCart";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Product } from "../interfaces/interface";
+import { ProductCart } from "./ProductCart";
 
 export default function Products() {
+  const { CartState } = useContext(CartContext);
+
+  const { products } = CartState;
   return (
     <div className="grid grid-cols-4 gap-x-10 gap-y-10 py-10 ">
-      {products?.map((product) => (
-        <ProductCart key={product.id} product={product} />
+      {products?.map((product: Product) => (
+        <ProductCart
+          key={product.id}
+          product={product}
+          id={""}
+          imgUrl={""}
+          name={""}
+          price={""}
+          rating={""}
+        />
       ))}
     </div>
   );
