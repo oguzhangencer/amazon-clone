635c82f49cdc81b2f3d5cf011143003bd0791891
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-19 12:41:33 +0300

    changing Cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,88 +1,104 @@
-import CartContext from "../context/CartContext";
+import { CartContext } from "../context/CartContext";
 import { IconStar } from "@tabler/icons";
 import { Card, Image, Text, Badge, Button } from "@mantine/core";
-import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
+import { Product } from "../interfaces/interface";
 
-export default function Cart() {
-  const { cartItems, deleteCart, addOrder, emptyCart } =
-    useContext(CartContext);
-  let navigate = useNavigate();
+interface Props {
+  id: string;
+  imgUrl: string;
+  name: string;
+  price: string;
+  rating: string;
+  product: Product;
+}
 
-  const handleOrder = async () => {
-    await addOrder(cartItems);
-    await emptyCart();
-    navigate("/order");
-  };
+export const Cart = ({ imgUrl, name, price, product, id, rating }: Props) => {
+  const { addToCart } = useContext(CartContext);
 
   return (
     <div className="grid grid-cols-4 gap-x-10 gap-y-10 py-10 ">
-      <div className="p-5 border-b ">
-        <Text className="font-bold text-2xl">Your Shopping Cart</Text>
-        <Text className="text-xl my-2">
-          Total: $
-          {cartItems.length > 0
-            ? Number(
-                cartItems.reduce((amount, item) => item.price + amount, 0)
-              ).toFixed(2)
-            : "0"}
-        </Text>
-        <Button
-          className="mb-1"
-          variant="light"
-          color="orange"
-          radius="xl"
-          size="lg"
-          onClick={handleOrder}
-          disabled={!cartItems.length > 0}
+      <div className="flex items-center">
+        <Card
+          className="drop-shadow-md flex flex-col items-center justify-center"
+          p="lg"
+          radius="md"
+          withBorder
         >
-          Order
-        </Button>
-      </div>
-      {cartItems?.map((cart, key) => (
-        <div key={key} className="flex items-center">
-          <Card
-            className="drop-shadow-md flex flex-col items-center justify-center"
-            p="lg"
-            radius="md"
-            withBorder
-          >
-            <Card.Section>
-              <Image src={cart.imgUrl} height="auto" />
-            </Card.Section>
+          <Card.Section>
+            <Image src={imgUrl} height="auto" />
+          </Card.Section>
 
-            <div className="mt-auto flex flex-col gap-y-2">
-              <Text className="text-lg" weight={500}>
-                {cart.name}
-              </Text>
-              <Badge color="pink" variant="light">
-                Include Stock
-              </Badge>
+          <div className="mt-auto flex flex-col gap-y-2">
+            <Text className="text-lg" weight={500}>
+              {name}
+            </Text>
+            <Badge color="pink" variant="light">
+              Include Stock
+            </Badge>
 
-              <div className="flex items-center justify-between">
-                <Text className="text-black text-2xl font-mono">
-                  ${cart.price}
-                </Text>
-                <div className="flex items-center justify-center space-x-1">
-                  <IconStar />
-                  <Text className="">{cart.rating}</Text>
-                </div>
+            <div className="flex items-center justify-between">
+              <Text className="text-black text-2xl font-mono">${price}</Text>
+              <div className="flex items-center justify-center space-x-1">
+                <IconStar />
+                <Text className="">{rating}</Text>
               </div>
-
-              <Button
-                onClick={() => deleteCart(cart.id)}
-                className="mb-1"
-                variant="light"
-                color="orange"
-                radius="xl"
-                size="lg"
-              >
-                Delete
-              </Button>
             </div>
-          </Card>
-        </div>
-      ))}
+
+            <Button
+              onClick={() => addToCart(product)}
+              className="mb-1"
+              variant="light"
+              color="orange"
+              radius="xl"
+              size="lg"
+            >
+              Delete
+            </Button>
+          </div>
+        </Card>
+      </div>
     </div>
   );
-}
+};
+
+// <Card
+//   className="drop-shadow-md flex flex-col items-center justify-center"
+//   p="lg"
+//   radius="md"
+//   withBorder
+// >
+//   <Card.Section>
+//     <Image src={product.imgUrl} height="auto" />
+//   </Card.Section>
+
+//   <div className="mt-auto flex flex-col gap-y-2">
+//     <Text className="text-lg" weight={500}>
+//       {product.name}
+//     </Text>
+//     <Badge color="pink" variant="light">
+//       Include Stock
+//     </Badge>
+
+//     <div className="flex items-center justify-between">
+//       <Text className="text-black text-2xl font-mono">
+//         ${product.price}
+//       </Text>
+//       <div className="flex items-center justify-center space-x-1">
+//         <IconStar />
+//         <Text className="">{product.rating}</Text>
+//       </div>
+//     </div>
+
+//     <Button
+//       onClick={() => addToCart(product)}
+//       className="mb-1"
+//       variant="light"
+//       color="orange"
+//       radius="xl"
+//       size="lg"
+//     >
+//       Add to Cart
+//     </Button>
+//   </div>
+// </Card>
