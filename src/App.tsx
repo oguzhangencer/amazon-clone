8a1bfe603824405e772c7aeff952556d6bdfb2eb
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-19 13:20:26 +0300

    Added Products

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,36 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Order from "./components/Order";
+import Products from "./components/Products";
+import { CartProvider } from "./context/CartProvider";
 import Cart from "./components/Cart";
+import { products } from "./data/products";
 
 export default function App() {
+  const { id, imgUrl, price, name } = products[0];
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      <Footer />
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                id={id}
+                images={imgUrl}
+                price={price}
+                title={name}
+                product={products[0]}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
   );
 }
