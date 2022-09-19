39d3b7f51f8bc01a8853a2e9921dcadbb0aaeaaa
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-09-19 12:43:42 +0300

    Cart Reducer

---
+++ src/context/cartReducer.ts
@@ -0,0 +1,26 @@
+import { CartState, Product } from "../interfaces/interface";
+
+type CartAction =
+| { type: "addProduct", payload: Product}
+| { type: "deleteProduct", payload: Product}
+
+export const CartReducer = (state: CartState, action: CartAction): => {
+  switch (action.type) {
+    case "addProduct":
+      return {
+        ...state,
+        productCount: state.productCount + 1,
+        products: [...state.products, action.payload],
+        total: Number(state.total) + Number(action.payload.price)
+      }
+    case "deleteProduct":
+      return {
+        ...state,
+        productCount: state.productCount - 1,
+        products: [...state.products.filter(product => product?.id !== action.payload?.id)],
+        total: Number(state.total) - Number(action.payload.price)
+      }
+    default:
+      return state
+  }
+}
